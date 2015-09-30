c01f302c5fd9549ab2da6df1d723286186f0527a
JP Wesselink <jp@jps-macbook-pro.cs.ad.klmcorp.net>
2015-09-30 16:52:02 +0200

    Tweaks to get bulkify working

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ var negate = require('lodash.negate');
 var isObject = require('lodash.isobject');
 var isUndefined = require('lodash.isundefined');
 var merge = require('lodash.merge');
-var bulk = require('bulk-require');
 var resolverRevolver = require('resolver-revolver');
 var util = require('util');
 var zipObject = require('lodash.zipobject');
 
 module.exports = function (options) {
+  var bulk = require('bulk-require');
   // patch our console
   var currentNodeEnv;
 
